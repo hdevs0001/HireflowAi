5ed5e4e05bc9556f97a5510f8981947f4cca1a37
Harman <hdevs0001@gmail.com>
2026-07-05 15:06:03 +0530

    add the provider and added the password filed in the schema(User)

--- hireflowai/auth.ts
+++ hireflowai/auth.ts
@@ -1,8 +1,19 @@
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { prisma } from "@/prisma"
- 
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/prisma";
+import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
-})
+  providers: [
+    Github({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+});
